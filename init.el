0fd12d580ed69c1e4caf4550fce06e6b3da18861
Kristian Lein-Mathisen <kristianlein@gmail.com>
2012-01-06 11:40:59 +0100

    initial emacs init.el and friends (everything that's not from elpa/package-install)

---
+++ init.el
@@ -0,0 +1,41 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+
+;load custom faces from a different file
+(setq custom-file "~/.emacs.d/custom.el")
+(load-file custom-file)
+
+(load-file "~/.emacs.d/colortheme-railscasts.el")
+
+; iedit replace feature
+(load-file "~/.emacs.d/iedit.el")
+(define-key global-map (kbd "C-;") 'iedit-mode)
+
+
+; document editing for racket (don't think i'm gonna be using this, though)
+(load-file "~/.racket/planet/300/5.2/cache/neil/scribble-emacs.plt/1/2/scribble.el")
+
+
+(load-file "~/.emacs.d/geiser/elisp/geiser.el")
+(require 'geiser)
+(setq geiser-mode-smart-tab-p t)
+
+; quack should be loaded after geiser
+(load-file "~/.emacs.d/quack.el")
+
+; I want to enable 'esk-paren-face' for list-modes, so that parens
+; don't stand out so much.
+(font-lock-add-keywords
+ 'scheme-mode
+ '(("(\\|)" . 'esk-paren-face)))
+
+(font-lock-add-keywords
+ 'lisp-mode
+ '(("(\\|)" . 'esk-paren-face)))
+
+(font-lock-add-keywords
+ 'emacs-lisp-mode
+ '(("(\\|)" . 'esk-paren-face)))
