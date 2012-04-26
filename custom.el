5166e053a2af018427395dcb770a0ea763f2a1ea
Kristian Lein-Mathisen <kristianlein@gmail.com>
2012-04-26 14:58:23 +0200

    misc updates

--- custom.el
+++ custom.el
@@ -4,7 +4,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(quack-default-program "racket"))
+ '(browse-url-browser-function (quote browse-url-chromium))
+ '(paren-mode (quote paren) nil (paren))
+ '(quack-default-program "csi")
+ '(quack-programs (quote ("adb shell /cache/chicken/csi.sh" "adb shell LD_LIBRARY_PATH=. /cache/chicken/csi" "bigloo" "cat" "cat" "csi" "csi -hygienic" "gosh" "gracket" "gsi" "gsi ~~/syntax-case.scm -" "guile" "kawa" "mit-scheme" "my-csi -:b" "nc localhost 1234" "racket" "racket -il typed/racket" "rs" "scheme" "scheme48" "scsh" "sisc" "stklos" "sxi" "valgrind csi" "valgrindcsi" "~/prog/android/native-activity/headers/box2d-2.2.1/box2d-test.scm" "~/testing")))
+ '(show-paren-delay 0)
+ '(show-paren-mode (quote paren) nil (paren)))
 
 
 (custom-set-faces
@@ -14,6 +19,19 @@
  ;; If there is more than one, they won't work right.
  '(default ((t (:background "#232323" :foreground "#e6e1de"))))
  '(highlight ((t (:background "grey20"))))
- '(link ((t (:foreground "#9999ff" :underline t))))
- '(magit-item-highlight ((t (:background "#303030")))))
+ '(info-node ((t (:bold t :underline t :foreground "goldenrod"))))
+ '(info-xref ((t (:bold t :underline t :foreground "#0000ee"))))
+ '(link ((t (:foreground "#5555ff" :underline t))))
+ '(magit-diff-del ((t (:foreground "#ff3333"))))
+ '(magit-diff-file-header ((t (:inherit magit-header :foreground "yellow"))))
+ '(magit-diff-hunk-header ((t (:foreground "#7777ff" :slant italic))))
+ '(magit-diff-none ((t (:foreground "grey40"))))
+ '(magit-item-highlight ((t (:background "#303030"))))
+ '(magit-log-graph ((t (:foreground "grey60"))))
+ '(paren-face-match ((((class color)) (:background "green"))))
+ '(paren-face-mismatch ((((class color)) (:foreground "white" :background "red"))))
+ '(paren-match ((t (:background "green"))))
+ '(paren-mismatch ((t (:background "red"))))
+ '(show-paren-match ((((class color)) (:background "green"))))
+ '(show-paren-mismatch ((((class color)) (:background "red")))))
 
--- init.el
+++ init.el
@@ -4,11 +4,9 @@
 (package-initialize)
 
 
-;load custom faces from a different file
-(setq custom-file "~/.emacs.d/custom.el")
-(load-file custom-file)
-
 (load-file "~/.emacs.d/colortheme-railscasts.el")
+
+(load-file "~/.emacs.d/bigloo.el")
 
 ; iedit replace feature
 (load-file "~/.emacs.d/custom/iedit.el")
@@ -16,12 +14,12 @@
 
 
 ; document editing for racket (don't think i'm gonna be using this, though)
-(load-file "~/.racket/planet/300/5.2/cache/neil/scribble-emacs.plt/1/2/scribble.el")
+;(load-file "~/.racket/planet/300/5.2/cache/neil/scribble-emacs.plt/1/2/scribble.el")
 
 
-(load-file "~/.emacs.d/geiser/elisp/geiser.el")
-(require 'geiser)
-(setq geiser-mode-smart-tab-p t)
+;(load-file "~/.emacs.d/geiser/elisp/geiser.el")
+;(require 'geiser)
+;(setq geiser-mode-smart-tab-p t)
 
 ; quack should be loaded after geiser
 (load-file "~/.emacs.d/custom/quack.el")
@@ -45,4 +43,47 @@
 (set-face-attribute 'default nil :height 90)
 
 
+(load-file "~/.emacs.d/custom/window-numbering.el")
+(window-numbering-mode 1)
 
+
+
+;load custom faces from a different file
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+(put 'ido-exit-minibuffer 'disabled nil)
+
+;; http://synthcode.com/wiki/scheme-complete
+(require 'scheme-complete)
+(autoload 'scheme-smart-complete "scheme-complete" nil t)
+(eval-after-load 'scheme
+  '(define-key scheme-mode-map "\t" 'scheme-complete-or-indent))
+
+;(setq lisp-indent-function 'scheme-smart-indent-function)
+
+
+
+
+
+
+(defun fullscreen ()
+  (interactive)
+  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+                         '(2 "_NET_WM_STATE_FULLSCREEN" 0)))
+
+;; doesnt work:
+(add-hook 'server-visit-hook
+          '(lambda ()
+             (message "run hook") (set-cursor-color "#FF55FF")))
+(add-hook 'server-done-hook
+          '(lambda ()
+             (message "server is done")))
+(add-hook 'server-switch-hook
+          '(lambda ()
+             (message "server switch hook")))
+
+(set-face-attribute 'cursor nil :background "#FF55FF")
+
+
+(add-to-list 'load-path "/usr/lib/chicken/6/")   ; Where Eggs are installed
+(autoload 'chicken-slime "chicken-slime" "SWANK backend for Chicken" t)
