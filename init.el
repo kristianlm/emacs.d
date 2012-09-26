ec2f50e5bde55c88ad965c673202560789624852
Kristian Lein-Mathisen <kristianlein@gmail.com>
2012-09-26 14:07:30 +0200

    added window-dedicated shortcut key
    kinda hacky but I like it

--- init.el
+++ init.el
@@ -80,3 +80,15 @@
 (set-face-attribute 'cursor nil :background "#FF55FF")
 
 
+
+;;; **** dedicated window toggle
+(defun toggle-current-window-dedication ()
+ (interactive)
+ (let* ((window    (selected-window))
+        (dedicated (window-dedicated-p window)))
+   (set-window-dedicated-p window (not dedicated))
+   (message "Window %sdedicated to %s" 
+            (if dedicated "no longer " "")
+            (buffer-name))))
+
+(global-set-key [pause] 'toggle-current-window-dedication)
